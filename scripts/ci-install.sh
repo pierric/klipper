9fe55ed50b2ba9587e646032ba0c7cac9478f0e7
Jiasen Wu <jiasenwu@hotmail.com>
2024-07-09 14:21:58 +0200

    remove py2 things

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -70,13 +70,3 @@ echo -e "\n\n=============== Install python3 virtualenv\n\n"
 cd ${MAIN_DIR}
 virtualenv -p python3 ${BUILD_DIR}/python-env
 ${BUILD_DIR}/python-env/bin/pip install -r ${MAIN_DIR}/scripts/klippy-requirements.txt
-
-
-######################################################################
-# Create python2 virtualenv environment
-######################################################################
-
-echo -e "\n\n=============== Install python2 virtualenv\n\n"
-cd ${MAIN_DIR}
-virtualenv -p python2 ${BUILD_DIR}/python2-env
-${BUILD_DIR}/python2-env/bin/pip install -r ${MAIN_DIR}/scripts/klippy-requirements.txt
